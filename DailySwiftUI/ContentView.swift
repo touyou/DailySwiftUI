042f767c1141b76878c1db062720a05ac6af75ca
touyou <fujiyou1121@gmail.com>
2019-06-07 14:56:47 +0900

    Initial Commit

---
+++ DailySwiftUI/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  DailySwiftUI
+//
+//  Created by 藤井陽介 on 2019/06/07.
+//  Copyright © 2019 touyou. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
