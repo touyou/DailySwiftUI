59ed2c7c3f058577c80bb6e1e594678104d72433
touyou <fujiyou1121@gmail.com>
2019-10-09 00:01:29 +0900

    change to zstack

--- DailySwiftUI/Sources/Day1/SignUp.swift
+++ DailySwiftUI/Sources/Day1/SignUp.swift
@@ -17,19 +17,40 @@ struct SignUp : View {
 
     var body: some View {
         VStack {
-            TextField("Email", text: $idString) {
-                print(self.idString)
-            }
-            SecureField("Password", text: $passString) {
-                print(self.passString)
-            }
-            Picker(selection: $modeNumber, label: Text("")) {
-                ForEach(0 ..< modes.count) {
-                    Text(self.modes[$0])
+            ZStack {
+                RoundedRectangle(cornerRadius: 8.0)
+                    .fill(/*@START_MENU_TOKEN@*/Color.white/*@END_MENU_TOKEN@*/)
+                    .shadow(radius: 4)
+                VStack {
+                    Spacer()
+                    TextField("Email", text: $idString) {
+                        print(self.idString)
+                    }
+                    .padding(.horizontal, 32.0)
+                    SecureField("Password", text: $passString) {
+                        print(self.passString)
+                    }
+                    .padding(.horizontal, 32.0)
+                    Spacer()
+                    Picker(selection: $modeNumber, label: Text("")) {
+                        ForEach(0 ..< modes.count) {
+                            Text(self.modes[$0])
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                    .padding(EdgeInsets(top: 0.0, leading: 32.0, bottom: 0.0, trailing: 32.0))
+                    Spacer()
                 }
             }
-            .pickerStyle(SegmentedPickerStyle())
-            .padding(EdgeInsets(top: 0.0, leading: 32.0, bottom: 0.0, trailing: 32.0))
+            .padding(.horizontal, 32.0)
+            .frame(height: 120, alignment: .center)
+            Spacer()
+            Button(action: {}) {
+                Text("Next")
+                    .font(.body)
+                    .fontWeight(.semibold)
+                    .multilineTextAlignment(.center)
+            }
         }
     }
 }
